c8a761f77eab609fd62f901257bd48b6572c2557
abdul rehman e <abdulrkhan570@gmail.com>
2023-03-12 17:38:21 +0530

    Level 4.1 security using hashing using bcrypt package and salting

--- app.mjs
+++ app.mjs
@@ -1,9 +1,10 @@
-import * as dotenv from 'dotenv';
-import md5 from 'md5';
-dotenv.config()
 import express from "express";
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
+// salting -> Adding random combination of letters to the password and then Hash it using the hash fucntion;
+// salting round -> number of times Hashing a hash + random letters(salt); 
 const url = "mongodb://127.0.0.1:27017/secrets";
 const app = express();
 app.use(express.static("public"));
@@ -24,28 +25,37 @@ app.get('/:pageName',(req,res)=>{
 })
 app.post("/register", (req,res)=>{
     const user = req.body.username;
-    const pass = md5(req.body.password);
-    const newUser = new Notes({// Level 1 encription just storing the username and password in Db;
-        email:user,
-        password:pass
-});
-    newUser.save();
-    res.render("secrets");
+    const pass = req.body.password;
+    // creating hash of the password and storing that in the Db;
+    bcrypt.hash(pass, saltRounds).then(function(hash) {
+        const newUser = new Notes({// Level 1 encription just storing the username and password in Db;
+            email:user,
+            password:hash
+            
+    });
+            newUser.save();
+            res.render("secrets");
+    });
+   
 })
 app.post("/login", (req,res)=>{
     const user = req.body.username;
-    const pass = md5(req.body.password);
+    const pass = req.body.password;
     Notes.findOne({email:user}).then(data=>{
+        console.log(data.password);
         if(data){
-            if(data.password === pass){
-                res.render("secrets");
-            }else{
-                res.redirect("/login");
-            }
+            // Comparing the Hash associated with that email stored in Db with the the entered password hash;
+            bcrypt.compare(pass, data.password).then(function(result) {
+            // result will give true if match else false;
+                if(result){
+                    res.render("secrets");
+                }else{
+                    res.redirect("/login");
+                }
+            });
         }else{
             res.redirect("/login");
         }
-        
     }).catch(err=>{
         console.log(err);
     })
